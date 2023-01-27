e125b663ff40c5f1409e00beeb07a57f86d96e4e
Konrad Koschel <konrad.koschel@sap.com>
2023-01-27 19:41:00 +0100

    Add Astro-Compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
+import compress from "astro-compress";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [mdx(), compress()]
 });
