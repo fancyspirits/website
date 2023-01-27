6a10119cd24704ee7f9003a269a9e2d43fb98b3e
Konrad Koschel <konrad.koschel@sap.com>
2023-01-27 20:44:50 +0100

    SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,17 @@ import mdx from "@astrojs/mdx";
 import compress from "astro-compress";
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+import robotsTxt from "astro-robots-txt";
+
+// https://astro.build/config
+import prefetch from "@astrojs/prefetch";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), compress()]
+  integrations: [mdx(), sitemap(), robotsTxt(), prefetch({
+    selector: "a[href^='/releases'], a[href^='/artists']"
+  }), compress()]
 });
