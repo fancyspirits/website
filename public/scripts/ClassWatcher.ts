24abcf1dba62e717358abda17fd057281a3dfa35
Konrad Koschel <konrad.koschel@sap.com>
2023-02-27 22:24:06 +0100

    Toggle Image Highlighting

---
+++ public/scripts/ClassWatcher.ts
@@ -0,0 +1,38 @@
+export class ClassWatcher {
+    observer: MutationObserver;
+    lastClassState: boolean;
+
+    constructor(private targetNode: HTMLElement, private classToWatch: string, private onRemove: () => void) {
+        this.observer = null;
+        this.lastClassState = targetNode.classList.contains(this.classToWatch);
+
+        this.init();
+    }
+
+    init() {
+        this.observer = new MutationObserver(this.mutationCallback.bind(this));
+        this.observe();
+    }
+
+    observe() {
+        this.observer.observe(this.targetNode, { attributes: true });
+    }
+
+    disconnect() {
+        this.observer.disconnect();
+    }
+
+    mutationCallback(mutationsList: MutationRecord[]) {
+        mutationsList.forEach(mutation => {
+            if (mutation.type === "attributes" && mutation.attributeName === "class") {
+                const currentClassState = (mutation.target as HTMLElement).classList.contains(this.classToWatch);
+                if (this.lastClassState !== currentClassState) {
+                    this.lastClassState = currentClassState;
+                    if (!currentClassState) {
+                        this.onRemove();
+                    }
+                }
+            }
+        })
+    }
+}
